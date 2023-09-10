ff2723565f1c0d6f2dfaac4dd77ee18ca75bd62f
CryoSnake22 <65683733+CryoSnake22@users.noreply.github.com>
2023-09-09 21:47:23 -0400

    add hover and on click animations to all buttons

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const calcContainer = document.querySelector("#numbers")
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach((element) => {
+    element.addEventListener("mousedown", function(){
+        element.style.backgroundColor = '#1C1C1C';
+    })
+    element.addEventListener("mouseout" || "mouseout", function(){
+        element.style.backgroundColor = '';
+        element.style.color = '';
+    })
+    element.addEventListener("mouseup" || "mouseout", function(){
+        element.style.backgroundColor = '';
+        element.style.color = '';
+    })
+})
+console.log(buttons)
